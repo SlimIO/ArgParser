be97cd4df482b07ee35722091ee2964ef60205ce
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2018-12-15 01:54:35 +0100

    perf: add benchmark

---
+++ benchmark/index.js
@@ -0,0 +1,40 @@
+const { argDefinition, parseArg } = require("../");
+
+// [De-]optimize method
+argDefinition("-p --product [number=10]", "Description");
+argDefinition("-p --product [number=10]", "Description");
+
+console.log("Testing argDefinition_t1 on 1,000 iteration!");
+console.time("argDefinition_t1");
+for (let id = 0; id < 1000; id++) {
+    argDefinition("-p --product [number=10]", "Description");
+}
+console.timeEnd("argDefinition_t1");
+
+console.log("\n\nTesting argDefinition_t2 on 1,000 iteration!");
+console.time("argDefinition_t2");
+for (let id = 0; id < 1000; id++) {
+    argDefinition("--product");
+}
+console.timeEnd("argDefinition_t2");
+
+console.log("\n\nTesting parsing_v1 on 1,000 iteration!");
+{
+    const defs = [
+        argDefinition("--product [number=10]"),
+        argDefinition("--verbose"),
+        argDefinition("-f --fastboot"),
+        argDefinition("-c --colors [array]")
+    ];
+    const argv = ["--product", "10", "-f", "--colors", "red", "blue", "yellow"];
+
+    // [De-]optimize method
+    parseArg(defs, argv);
+    parseArg(defs, argv);
+
+    console.time("parsing_v1");
+    for (let id = 0; id < 1000; id++) {
+        parseArg(defs, argv);
+    }
+    console.timeEnd("parsing_v1");
+}
