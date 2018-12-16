a93928a9dc6e13704a9ed5ed4c97b1e1b583c23a
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2018-12-16 01:29:30 +0100

    refactor: improve typescript def

--- example/replica_agent.js
+++ example/replica_agent.js
@@ -1,5 +1,12 @@
 const { parseArg, argDefinition } = require("../");
 
+/**
+ * @typedef {Object} Argv
+ * @property {Boolean} verbose
+ * @property {Number} autoreload
+ */
+
+/** @type {!ArgParser.ArgvResult<Argv>} */
 const result = parseArg([
     argDefinition("--verbose", "Enable verbose mode!"),
     argDefinition("-a --autoreload [number=500]", "Configuration Autoreload delay in number")
--- index.d.ts
+++ index.d.ts
@@ -1,17 +1,18 @@
 /// <reference types="node" />
 
 declare namespace ArgParser {
-    interface Command {
+    export interface Command {
         name: string;
         type: string;
         description?: string;
         shortcut?: string;
         defaultVal?: number | string | boolean | any[];
     }
+
     export type ArgvResult<T> = Map<keyof T, T[keyof T]>;
 
-    export function argDefinition(cmd: string, description?: string): Command;
-    export function parseArg<T>(argDefinitions?: Command[], argv?: string[]): ArgvResult<T>;
+    export function argDefinition(cmd: string, description?: string): ArgParser.Command;
+    export function parseArg<T>(argDefinitions?: ArgParser.Command[], argv?: string[]): ArgParser.ArgvResult<T>;
 }
 
 export as namespace ArgParser;
