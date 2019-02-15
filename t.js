c0eb41f5d0f97f55040ca45ea86842c5f87425a7
Malaj Alexandre <alexandre.malaj@gmail.com>
2019-02-16 00:39:47 +0100

    chore: remove t.js

--- t.js
+++
@@ -1,9 +0,0 @@
-const ArgParser = require("./");
-
-const cmdDef = [
-    ArgParser.argDefinition("-p --product [number=10]", "Product number description"),
-    ArgParser.argDefinition("-t --truc [string]"),
-    ArgParser.argDefinition("--bidule")
-];
-
-ArgParser.help(cmdDef);
